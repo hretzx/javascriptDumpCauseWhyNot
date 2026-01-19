ccd8db6ff4d51278487b56f124c390c5a37e5cb5
Hritvi Narvekar <hritvi.narvekar@vit.edu.in>
2026-01-19 20:00:37 +0530

    ok

---
+++ thirdProject/script.js
@@ -0,0 +1,12 @@
+const buttons= document.querySelectorAll('button');
+const result = document.querySelector('.result');
+buttons.forEach(function(button){
+    button.addEventListener('click',function(e){
+        if(e.target.id==="op1" || e.target.id==="op2" || e.target.id==="op3"){
+            result.innerHTML='Wrong option';
+        }
+        else{
+            result.innerHTML='Correct';
+        }
+    });
+});
